978421d549ec6fe0d7bcf8d1d4cd40841503619f
jenna cooper and kimlan nguyen <jenna+kimlan@>
2017-06-28 15:13:43 -0700

    Add api, call request

--- js/backend.js
+++ js/backend.js
@@ -1,4 +1,3 @@
-(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 function Bike(manufacturer, color , location, distance, stolenness){
   if(manufacturer == "undefined"){
     this.Manufacturer = null;
@@ -51,28 +50,3 @@ Bike.prototype.getBikes = function(manufacturer, color, location, distance, stol
 };
 
 exports.bikeModule = Bike;
-
-},{}],2:[function(require,module,exports){
-var Bike = require('./../js/backend.js').bikeModule;
-var displayInfo = function(title, serial, manufacturer_name){
-  $("#display").append("<li>Title: " + title + "</li>" + "<li>Serial: " + serial + "</li>" + "<li>Manufacturer: " + manufacturer_name + "</li>" );
-};
-
-
-$(document).ready(function(){
-  $("form#search-form").submit(function(event){
-    event.preventDefault();
-    $("#display").empty();
-    var manufacturer = $("#manufacturer").val();
-    var color = $("#color").val();
-    var distance = $("#distance").val();
-    var location = $("#location").val();
-    var stolenness = $("#stolenness").val();
-    var Bikes = new Bike(manufacturer, color, location, distance, stolenness);
-
-    Bikes.getBikes(Bikes.Manufacturer, Bikes.Color, Bikes.Location, Bikes.Distance, Bikes.Stolenness, displayInfo);
-  });
-
-});
-
-},{"./../js/backend.js":1}]},{},[2]);
